82a42d23c76d691df67901b0ec3c5cfece1e5305
karimaljundi <jundiak@gmail.com>
2024-11-16 22:16:12 -0500

    create spring boot application and connect it to my postgres database containing the player csv

--- scrape_players.py
+++ scrape_players.py
@@ -9,17 +9,17 @@ from selenium import webdriver
 
 years = list(range(1990, 2024))
 
-player_stat_url = "https://www.basketball-reference.com/leagues/NBA_{}_per_game.html"
-driver = webdriver.Chrome()
-for year in years:
-    url = player_stat_url.format(year)
-    driver.get(url)
-    driver.execute_script("window.scrollTo(1,10000)")
-    time.sleep(2)
+# player_stat_url = "https://www.basketball-reference.com/leagues/NBA_{}_per_game.html"
+# driver = webdriver.Chrome()
+# for year in years:
+#     url = player_stat_url.format(year)
+#     driver.get(url)
+#     driver.execute_script("window.scrollTo(1,10000)")
+#     time.sleep(2)
 
-    html = driver.page_source
-    with open("players/{}.html".format(year), "w+") as f:
-            f.write(html)
+#     html = driver.page_source
+#     with open("players/{}.html".format(year), "w+") as f:
+#             f.write(html)
 
 df = []
 for year in years:
@@ -32,6 +32,16 @@ for year in years:
     player["Year"] = year
     df.append(player)
 players = pd.concat(df)
+
+# Remove duplicate columns, keeping the first occurrence
+players = players.loc[:, ~players.columns.duplicated()]
+
+# Remove the last row if it contains unwanted text
+players = players[~players['Player'].str.contains("league average", na=False)]
+
+# Keep only the latest instance of each player
+players = players.sort_values(by='Year').drop_duplicates(subset='Player', keep='last')
+
 players.to_csv("players.csv")
     
 
