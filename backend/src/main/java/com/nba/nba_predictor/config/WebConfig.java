f490405b06b925cbeec12bb3310d7a7802e162ec
karimaljundi <jundiak@gmail.com>
2024-11-24 01:54:02 -0500

    Made CORS exceptions to static website

--- backend/src/main/java/com/nba/nba_predictor/config/WebConfig.java
+++ backend/src/main/java/com/nba/nba_predictor/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/v1/player/**")
-                .allowedOrigins("http://localhost:3000") // Allow your React app's origin
+                .allowedOrigins("http://nba-player-directory-bucket.s3-website.us-east-2.amazonaws.com") // Allow your React app's origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
