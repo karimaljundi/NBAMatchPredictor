bb60aab52255c2af83066efcd3ffb14f343f19a7
karimaljundi <jundiak@gmail.com>
2024-11-24 21:47:27 -0500

    Fixed CORS to function locally and on the cloud

--- backend/src/main/java/com/nba/nba_predictor/config/WebConfig.java
+++ backend/src/main/java/com/nba/nba_predictor/config/WebConfig.java
@@ -9,9 +9,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/v1/player/**")
-                .allowedOrigins("http://nba-player-directory-bucket.s3-website.us-east-2.amazonaws.com") // Allow your React app's origin
+        registry.addMapping("/api/**")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "http://nba-player-directory-bucket.s3-website.us-east-2.amazonaws.com"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
+
     }
 }
